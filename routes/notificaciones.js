23efd2a9ea3089900e9d70491df2772a151e96bd
Michael <rmworm18@gmail.com>
2026-05-07 13:53:05 -0500

    notificaciones

--- routes/notificaciones.js
+++ routes/notificaciones.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const notiCtrl = require('../controllers/notificacionController');
 const authenticate = require('../middlewares/auth');
+const authorize = require('../middlewares/authorize');
+const ownership = require('../middlewares/ownership');
 
-router.get('/user/:userId', authenticate, notiCtrl.listForUser);
+router.get('/user/:userId', authenticate, authorize('Administrador', 'Instructor', 'Aprendiz', 'Visitante', 'Vigilante'), ownership({ source: 'params', field: 'userId', authUserField: 'id', allowRoles: ['Administrador'] }), notiCtrl.listForUser);
 router.patch('/:id/read', authenticate, notiCtrl.markRead);
 
 module.exports = router;
